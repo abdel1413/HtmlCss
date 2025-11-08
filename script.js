551e5aac19acab6366bdbec65110a45e534560fe
aboulayetchakoura <aboulayet63@gmail.com>
2025-11-08 13:22:39 -0500

    learning design

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById('theme-toggle').addEventListener('click', () => {
+  document.querySelector('.design').classList.toggle('dark');
+});
+
+
+document.querySelector('.saturation-btn').addEventListener('click', () => {
+  document.querySelector('.saturation-container').classList.toggle('dark');
+});
